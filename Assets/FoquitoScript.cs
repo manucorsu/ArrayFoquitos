76af5c0a9d31b5774219b2896fe8a256e8fad039
manucorsu <142500898+manucorsu@users.noreply.github.com>
2024-05-30 10:07:42 -0300

    Import package

---
+++ Assets/FoquitoScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoquitoScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] colors;
+    public int currentLightIndex =-1;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivateNextLight()
+    {
+        currentLightIndex++;
+        if (currentLightIndex >= colors.Length)
+        {
+            currentLightIndex = 0;
+        }
+        DeactivateAllLights();
+        colors[currentLightIndex].SetActive(true);
+    }
+
+    public void ActivatePreviousLight()
+    {
+        currentLightIndex--;
+        if (currentLightIndex < 0)
+        {
+            currentLightIndex = colors.Length - 1;
+        }
+        DeactivateAllLights();
+        colors[currentLightIndex].SetActive(true);
+    }
+
+    void DeactivateAllLights()
+    {
+        foreach (GameObject g in colors)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    public void ActivateRepeating(float t)
+    {
+        InvokeRepeating(nameof(ActivateNextLight),0,t);
+    }
+}
